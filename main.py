e3f4bf152366826bd86bcc7696616ef4a7f7becf
Graeme Danielson <graeme.danielson@netspan.co.nz>
2022-12-30 21:06:14 +1300

    Upd GH Actions minor upd fix naming etc.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pulumi_eks as eks
 
 project_name = pulumi.get_project()
 
-# Create an EKS cluster with the default configuration.
 cluster = eks.Cluster(
     f"{project_name}-1",
     instance_type="t3.micro",
